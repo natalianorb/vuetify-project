34971a9dea5bb97040ebeae3c0c166822efeb995
Natalya Norboeva <nata.norboeva@gmail.com>
2025-03-02 22:15:17 +0900

    go home button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <v-app>
+    <v-app-bar
+      flat
+      density="compact"
+      scroll-behavior="collapse"
+    >
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-btn
+          to="/"
+          icon="$vuetify"
+        />
+
+        <v-app-bar-title class="pa-3">
+          Vuetify Shop Project
+        </v-app-bar-title>
+      </v-container>
+    </v-app-bar>
+
     <router-view />
   </v-app>
 </template>
